0b0314965778f2a19f35c6fa7164474a07a76502
rezaul <rezaul.cse.mbstu@gmail.com>
2020-06-18 17:05:24 +0600

    paired with php package generator

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Neodynamic\SDK\Web\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+// TODO write test for sdk
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
